3742dc29e14b1415eca69e091256e80a24b1f468
jiri.fait <fait@orkasolutions.cz>
2021-04-22 19:09:28 +0200

    Funkcni API

--- api/views.py
+++ api/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import action
 from .models import Movie, Rating
 from .serializers import MovieSerializer, RatingSerializer
+from django.contrib.auth.models import User
 
 
 class MovieViewSet(viewsets.ModelViewSet):
@@ -11,8 +12,28 @@ class MovieViewSet(viewsets.ModelViewSet):
 
     @action(detail=True, methods=['POST'])
     def rate_movie(self, request, pk=None):
-        response = {'message': 'jo funguji'}
-        return Response(response, status=status.HTTP_200_OK)
+        if 'stars' in request.data:
+            movie = Movie.objects.get(id=pk)
+            stars = request.data['stars']
+            user = User.objects.get(id=1)
+
+            try:
+                rating = Rating.objects.get(user=user.id, movie=movie.id)
+                rating.stars = stars
+                rating.save()
+                serializer = RatingSerializer(rating, many=False)
+                response = {'message': 'Rating update', 'result': serializer.data}
+                return Response(response, status=status.HTTP_200_OK)
+            except:
+                rating = Rating.objects.create(user=user, movie=movie, stars=stars)
+                serializer = RatingSerializer(rating, many=False)
+                response = {'message': 'Rating create', 'result': serializer.data}
+                return Response(response, status=status.HTTP_200_OK)
+
+
+        else:
+            response = {'message': 'Musis zadat hodnoceni'}
+            return Response(response, status=status.HTTP_400_BAD_REQUEST)
 
 
 class RatingViewSet(viewsets.ModelViewSet):
